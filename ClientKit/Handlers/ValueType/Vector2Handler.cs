e77a73ba1fb7f74835e9bbaf0513491b62ad68e3
yueChin <transuni@foxmail.com>
2022-11-11 00:35:44 +0800

    Handlers/ValueType/Vector2Handler update

--- ClientKit/Handlers/ValueType/Vector2Handler.cs
+++ ClientKit/Handlers/ValueType/Vector2Handler.cs
@@ -25,44 +25,124 @@ namespace Kits.ClientKit.Handlers.ValueType
         
         public static Vector2 HoldX(this Vector2 v2)
         {
-            return new Vector2(v2.x, 0);
+            v2.y = 0;
+            return v2;
         }
 
         public static Vector2 HoldY(this Vector2 v2)
         {
-            return new Vector2(0, v2.y);
+            v2.x = 0;
+            return v2;
         }
 
-        public static Vector2 MergeX(this Vector2 v2,Vector2 v2T)
+        public static Vector2 MergeV2XToX(this Vector2 v2,Vector2 v2T)
         {
-            return new Vector2(v2T.x, v2.y);
+            v2.x = v2T.x;
+            return v2;
         }
         
-        public static Vector2 MergeX(this Vector2 v2,Vector3 v3T)
+        public static Vector2 MergeV2YToX(this Vector2 v2,Vector2 v2T)
         {
-            return new Vector2(v3T.x, v2.y);
+            v2.x = v2T.y;
+            return v2;
         }
         
-        public static Vector2 MergeX(this Vector2 v2,Vector4 v4T)
+        public static Vector2 MergeV3XToX(this Vector2 v2,Vector3 v3T)
         {
-            return new Vector2(v4T.x, v2.y);
+            v2.x = v3T.x;
+            return v2;
         }
         
-        public static Vector2 MergeY(this Vector2 v2,Vector2 v2T)
+        public static Vector2 MergeV3YToX(this Vector2 v2,Vector3 v3T)
         {
-            return new Vector2(v2.x, v2T.y);
+            v2.x = v3T.y;
+            return v2;
         }
         
-        public static Vector2 MergeY(this Vector2 v2,Vector3 v3T)
+        public static Vector2 MergeV3ZToX(this Vector2 v2,Vector3 v3T)
         {
-            return new Vector2(v2.x, v3T.y);
+            v2.x = v3T.z;
+            return v2;
         }
         
-        public static Vector2 MergeY(this Vector2 v2,Vector4 v4T)
+        public static Vector2 MergeV4XToX(this Vector2 v2,Vector4 v4T)
         {
-            return new Vector2(v2.x, v4T.y);
+            v2.x = v4T.x;
+            return v2;
         }
         
+        public static Vector2 MergeV4YToX(this Vector2 v2,Vector4 v4T)
+        {
+            v2.x = v4T.y;
+            return v2;
+        }
+        
+        public static Vector2 MergeV4ZToX(this Vector2 v2,Vector4 v4T)
+        {
+            v2.x = v4T.z;
+            return v2;
+        }
+        
+        public static Vector2 MergeV4WToX(this Vector2 v2,Vector4 v4T)
+        {
+            v2.x = v4T.w;
+            return v2;
+        }
+        
+        public static Vector2 MergeV2XToY(this Vector2 v2,Vector2 v2T)
+        {
+            v2.y = v2T.x;
+            return v2;
+        }
+        
+        public static Vector2 MergeV2YToY(this Vector2 v2,Vector2 v2T)
+        {
+            v2.y = v2T.y;
+            return v2;
+        }
+        
+        public static Vector2 MergeV3XToY(this Vector2 v2,Vector3 v3T)
+        {
+            v2.y = v3T.x;
+            return v2;
+        }
+        
+        public static Vector2 MergeV3YToY(this Vector2 v2,Vector3 v3T)
+        {
+            v2.y = v3T.y;
+            return v2;
+        }
+        
+        public static Vector2 MergeV3ZToY(this Vector2 v2,Vector3 v3T)
+        {
+            v2.y = v3T.z;
+            return v2;
+        }
+        
+        public static Vector2 MergeV4XToY(this Vector2 v2,Vector4 v4T)
+        {
+            v2.y = v4T.x;
+            return v2;
+        }
+        
+        public static Vector2 MergeV4YToY(this Vector2 v2,Vector4 v4T)
+        {
+            v2.y = v4T.y;
+            return v2;
+        }
+        
+        public static Vector2 MergeV4ZToY(this Vector2 v2,Vector4 v4T)
+        {
+            v2.y = v4T.z;
+            return v2;
+        }
+        
+        public static Vector2 MergeV4WToY(this Vector2 v2,Vector4 v4T)
+        {
+            v2.y = v4T.w;
+            return v2;
+        }
+
         public static Vector2 Y2Zero(this Vector2 v2)
         {
             return v2.ChangeY(0);
@@ -78,66 +158,98 @@ namespace Kits.ClientKit.Handlers.ValueType
             return (a.x * b.y - b.x * a.y);
         }
         
-        public static Vector3 ChangeX(this Vector2 v3, float x,VectorOperator vOp = VectorOperator.Equal)
+        public static Vector2 ChangeX(this Vector2 v2, float x,VectorOperator vOp = VectorOperator.Equal)
         {
             switch (vOp)
             {
                 case VectorOperator.Equal:
-                    return new Vector3(x, v3.y);
+                    v2.x = x;
+                    return v2;
                 case VectorOperator.Add:
-                    return new Vector3(v3.x + x, v3.y);
+                    v2.x += x;
+                    return v2;
                 case VectorOperator.Sub:
-                    return new Vector3(v3.x - x, v3.y);
+                    v2.x -= x;
+                    return v2;
                 case VectorOperator.Multi:
-                    return new Vector3(v3.x * x, v3.y);
+                    v2.x *= x;
+                    return v2;
                 case VectorOperator.Divide:
-                    return new Vector3(v3.x / x, v3.y);
+                    v2.x /= x;
+                    return v2;
                 case VectorOperator.Mud:
-                    return new Vector3(v3.x % x, v3.y);
+                    v2.x %= x;
+                    return v2;
                 default:
-                    return new Vector3(x, v3.y);
+                    v2.x = x;
+                    return v2;
             }
         }
 
-        public static Vector3 ChangeY(this Vector2 v3, float y,VectorOperator vOp = VectorOperator.Equal)
+        public static Vector2 ChangeY(this Vector2 v2, float y,VectorOperator vOp = VectorOperator.Equal)
         {
             switch (vOp)
             {
                 case VectorOperator.Equal:
-                    return new Vector3(v3.x, y);
+                    v2.x = y;
+                    return v2;
                 case VectorOperator.Add:
-                    return new Vector3(v3.x, v3.y + y);
+                    v2.x += y;
+                    return v2;
                 case VectorOperator.Sub:
-                    return new Vector3(v3.x, v3.y - y);
+                    v2.x -= y;
+                    return v2;
                 case VectorOperator.Multi:
-                    return new Vector3(v3.x, v3.y * y);
+                    v2.x *= y;
+                    return v2;
                 case VectorOperator.Divide:
-                    return new Vector3(v3.x, v3.y / y);
+                    v2.x /= y;
+                    return v2;
                 case VectorOperator.Mud:
-                    return new Vector3(v3.x, v3.y % y);
+                    v2.x %= y;
+                    return v2;
                 default:
-                    return new Vector3(v3.x, y);
+                    v2.x = y;
+                    return v2;
             }
         }
 
-        public static Vector2 Yx(this Vector2 v)
+        public static Vector2 YX(this Vector2 v2)
         {
-            return new Vector2(v.y, v.x);
+            float value = v2.y;
+            v2.x = v2.y;
+            v2.y = value;
+            return v2;
         }
         
-        public static Vector3 V2ToV3EmptyX(Vector2 v)
+        public static Vector3 V2ToV3EmptyX(this Vector2 v2)
         {
-            return new Vector3(0.0f,v.x, v.y);
+            return new Vector3(0, v2.x, v2.y);
         }
         
-        public static Vector3 V2ToV3EmptyY(Vector2 v)
+        public static Vector3 V2ToV3EmptyY(this Vector2 v2)
         {
-            return new Vector3(v.x, 0.0f, v.y);
+            return new Vector3(v2.x, 0, v2.y);
         }
         
-        public static Vector3 V2ToV3EmptyZ(Vector2 v)
+        public static Vector3 V2ToV3EmptyZ(this Vector2 v2)
         {
-            return new Vector3(v.x, v.y, 0.0f);
+            return new Vector3(v2.x, v2.y, 0);
+        }
+        
+        public static Vector4 V2ToV4EmptyXY(this Vector2 v2)
+        {
+            return new Vector4(0, 0, v2.x, v2.y);
+        }
+        
+        public static Vector4 V2ToV4EmptyXZ(this Vector2 v2)
+        {
+            return new Vector4(v2.x, 0, 0, v2.y);
+        }
+        
+        public static Vector4 V2ToV4EmptyXW(this Vector2 v2)
+        {
+            return new Vector4(0, v2.x, v2.y, 0);
         }
         
         public static bool Approximately(this Vector2 v2,Vector2 v2A,float delta = 0)
